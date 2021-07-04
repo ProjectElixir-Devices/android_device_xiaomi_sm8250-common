4d3c40925f0f5289d7d0c45521857774f22681b2
Ramii Ahmed <ramy@ahmedramy.com>
2021-07-04 12:53:33 +0000

    sm8250-common: Initialize High Touch Polling Rate support
    Used to handle our node to force enable Xiaomi's High Touch polling rate mode from kernel
    
    Adapted High Touch Polling Rate support to S style
    
    Change-Id: I50573ce3761104ada0e58d795b661ae8232a87f1

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,6 +25,7 @@ import android.util.Log;
 
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -35,6 +36,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
+        RefreshUtils.startService(context);
+        TouchSamplingUtils.restoreSamplingValue(context);
     }
 }
